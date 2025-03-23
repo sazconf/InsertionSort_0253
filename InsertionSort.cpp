9d679ea8b452caffe955f4c00cf2b78b842ad3fe
sazconf <md.sazzad.ft24@mail.umy.ac.id>
2025-03-23 11:19:31 -0400

    Take input and set condition max 20

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -4,3 +4,20 @@ using namespace std;
 int arr[20]: //Membuat Array dengan panjang data 20
 int n; //Membuat Variable inputan n
 
+void input()
+{//Procedure Input
+    while(true)
+    {
+        cout<< "Masukkan Jumlah Data pada Array:"; //Membuat Inputan jumlah element Array
+        cin>>n; //memanggil variable inputan n
+
+        if (n<=20)
+        {//Membuat kondisi n tidak lebih dari 20
+            break;
+        }
+        else{
+            cout << "\n Array yang anda masukkan maksimal 20 elemen.\n";//Menampilka pesan jika data lebih dari 20
+        }
+    }
+    
+}
